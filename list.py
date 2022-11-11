ba4d42acc28d19f3035f449e15e9209d9607dd71
Tim <timpham70@gmail.com>
2022-11-11 00:50:18 -0800

    Added class and files

---
+++ list.py
@@ -0,0 +1,9 @@
+#List setups
+
+class List:
+    def hello(self):
+        print("Hello!")
+
+def start():
+    obj = List()
+    obj.hello()
---
+++ main.py
@@ -0,0 +1,13 @@
+#Tim Pham
+#Check list Project with Python
+from list import *
+
+def main():
+    print("Check list Project")
+    obj = List()
+    obj.hello()
+
+
+if __name__ == "__main__":
+    main()
+    
