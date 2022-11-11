fd1d59e92931161802326b865df6c48df2080bcb
Tim <timpham70@gmail.com>
2022-11-11 06:19:22 -0800

    User Interface

--- list.py
+++ list.py
@@ -14,13 +14,15 @@ class List:
         self.character.append("word")
     def remove(self, number):
         print("Remove")
-        word = self.character[number]
+        word = self.character[number-1]
         self.character.remove(word)
     def display(self):
         print("Display")
         i = 0
         while(i < len(self.character)):
             if(self.character[i]):
-                print(self.character[i])
+                print(i+1,". ",self.character[i])
+                
             i += 1
+        print("")
 
--- main.py
+++ main.py
@@ -5,12 +5,35 @@ from list import *
 def main():
     print("Check list Project")
     obj = List()
-    obj.hello()
-    word = "Hello World!"
-    obj.insert(word)
-    obj.display()
-    obj.remove(1)
-    obj.display()
+    user = 0
+    word = ""
+    number = 0 
+    i = 0
+    print("Welcome, enter")
+    while(i != 4):
+        print("1 - Insert")
+        print("2 - Remove")
+        print("3 - Display")
+        print("4 - Exit")
+        
+        user = int(input("Enter command:"))
+        i = user
+        if(user == 1):
+            print("What do you want to add to the list?")
+            word = input("")
+            obj.insert(word)
+            word = ""
+            obj.display()
+        if(user == 2):
+            print("What do you want to remove from the list?")
+            number = int(input("Enter the number the topic is in: "))
+            obj.remove(number)
+            obj.display()
+        if(user == 3):
+            obj.display()
+            
+
+    
 
 if __name__ == "__main__":
     main()
