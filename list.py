79bb2671b58405314171bdcd98c0d8f7ed048756
Tim <timpham70@gmail.com>
2022-11-11 05:47:39 -0800

    Added class and files

--- list.py
+++ list.py
@@ -1,9 +1,25 @@
 #List setups
 
 class List:
+
+    character = []
+
+
     def hello(self):
         print("Hello!")
+    def insert(self, word):
+        print("Insert")
+        self.character.append(word)
+        self.character.append("word")
+        self.character.append("word")
+    def remove(self, number):
+        print("Remove")
+        word = self.character[number]
+        self.character.remove(word)
+    def display(self):
+        i = 0
+        while(i < len(self.character)):
+            if(self.character[i]):
+                print(self.character[i])
+            i += 1
 
-def start():
-    obj = List()
-    obj.hello()
--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def main():
     print("Check list Project")
     obj = List()
     obj.hello()
-
+    word = "Hello World!"
+    obj.insert(word)
+    obj.display()
+    obj.remove(1)
+    obj.display()
 
 if __name__ == "__main__":
     main()
